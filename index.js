e46097f68bf57ac91451b5aae67d58141d3b8cae
Sudipto Ghosh <sudipto@ghosh.pro>
2021-03-26 19:19:58 +0530

    make outbound calls

---
+++ index.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+const Vonage = require('@vonage/server-sdk');
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+const vonage = new Vonage({
+  apiKey: process.env.VONAGE_API_KEY,
+  apiSecret: process.env.VONAGE_API_SECRET,
+  applicationId: process.env.VONAGE_APPLICATION_ID,
+  privateKey: process.env.VONAGE_PRIVATE_KEY_PATH
+});
+
+app.use(morgan('tiny'));
+app.use(express.json());
+
+app.get('/call', (req, res) => {
+  vonage.calls.create({
+    to: [{
+      type: 'phone',
+      number: req.query.to || process.env.TO_NUMBER
+    }],
+    from: {
+      type: 'phone',
+      number: process.env.VONAGE_NUMBER,
+    },
+    ncco: [{
+      action: 'talk',
+      text: req.query.msg || 'This call was made from JavaScript.',
+      language: 'en-IN',
+      style: '4'
+    }]
+  }, (err, resp) => {
+    if (err)
+      console.error(err);
+    if (resp)
+      console.log(resp);
+  });
+  res.json('ok');
+});
+
+app.post('/event', (req, res) => {
+  console.log(req.body);
+  res.status(200).send('');
+});
+
+app.listen(process.env.PORT, () => console.log(`Running on port ${process.env.PORT}`));
